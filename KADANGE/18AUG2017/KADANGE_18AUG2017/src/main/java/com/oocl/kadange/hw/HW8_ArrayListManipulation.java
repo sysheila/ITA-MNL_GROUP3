44089526303db6816dfe93d212757777468dfa0d
kadange <gskadano@gmail.com>
2017-08-20 19:11:45 +0800

    18AUG (HW) - Update on arraylist duplicates

--- KADANGE/18AUG2017/KADANGE_18AUG2017/src/main/java/com/oocl/kadange/hw/HW8_ArrayListManipulation.java
+++ KADANGE/18AUG2017/KADANGE_18AUG2017/src/main/java/com/oocl/kadange/hw/HW8_ArrayListManipulation.java
@@ -1,26 +1,61 @@
 package com.oocl.kadange.hw;
 
+import java.io.Console;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class HW8_ArrayListManipulation {
 
 	public static void main(String[] args) {
-		ArrayList<String> arrayList = new ArrayList<String>();
-		for(int i=1; i<5; i++) {
-			if(i==3) {
-				arrayList.add("Test"+(i-1));
-			}
-			arrayList.add("Test"+i);
-		}
-		System.out.println(arrayList.toString());
-		ArrayList<String> newArrayList = removeDuplicate(arrayList);
+		//Get inputs
+		ArrayList<String> words = getInput();
+		System.out.println(words.toString());
+		
+		//Remove duplicates
+		ArrayList<String> newArrayList = removeDuplicate(words);
 		System.out.println(newArrayList.toString());
+	}
+	
+	private static ArrayList<String> inputWords(int numOfWords){
+		ArrayList<String> wordList = new ArrayList<String>();
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<numOfWords; i++) {
+			System.out.print("Enter word number "+(i+1)+": ");
+			String word="";
+			try {
+				word = new String(sc.nextLine().getBytes(), "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+			wordList.add(word);
+		}
+		return wordList;
+	}
+	
+	private static ArrayList<String> getInput() {
+		ArrayList<String> inputs = new ArrayList<String>();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter number of words: ");
+		if(sc.hasNextInt()) {
+			int numWords = sc.nextInt();
+			inputs = inputWords(numWords);
+		}else {
+			String input = sc.nextLine();
+			if(!input.toLowerCase().equals("exit")) {
+				System.out.println("Input is not a number. \nEnter \"exit\" to exit the program.");
+				inputs = getInput();
+			}
+		}
+		return inputs;
 	}
 	
 	public static ArrayList<String> removeDuplicate(ArrayList<String> arrayList) {
 		ArrayList<String> newArrayList = new ArrayList<String>();
-		Collections.sort(arrayList);
+		//Collections.sort(arrayList);
 		for(int i=0; i<arrayList.size(); i++) {
 			Boolean isExist=false;
 			if(newArrayList.isEmpty()) {
