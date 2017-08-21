f64ed5985ceaace343446143aa47ce43b8a305eb
daluplo <lsdalupan@github.com>
2017-08-21 09:21:59 +0800

    add hw

---
+++ DALUPLO/18AUG2017/DALUPLO_18AUG2017/src/main/java/com/oocl/daluplo/hw6/DeadlockHandler.java
@@ -0,0 +1,31 @@
+package com.oocl.daluplo.hw6;
+
+import java.lang.management.ThreadInfo;
+import java.util.Map;
+
+public class DeadlockHandler{
+
+	  public void handleDeadlock(final ThreadInfo[] deadlockedThreads) {
+	    if (deadlockedThreads != null) {
+	      System.err.println("Deadlock detected!");
+	      
+	      Map<Thread, StackTraceElement[]> stackTraceMap = Thread.getAllStackTraces();
+	      for (ThreadInfo threadInfo : deadlockedThreads) {
+	      
+	        if (threadInfo != null) {
+	      
+	          for (Thread thread : Thread.getAllStackTraces().keySet()) {
+	        
+	            if (thread.getId() == threadInfo.getThreadId()) {
+	              System.err.println(threadInfo.toString().trim());
+	                
+	              for (StackTraceElement ste : thread.getStackTrace()) {
+	                  System.err.println("\t" + ste.toString().trim());
+	              }
+	            }
+	          }
+	        }
+	      }
+	    }
+	  }
+	}
