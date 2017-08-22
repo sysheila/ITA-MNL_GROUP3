46a3de435e088db20ee277bd19d86d7cbe59c6a5
nixdasmarinas <mddasmarinas@up.edu.ph>
2017-08-22 18:15:50 +0800

    logging and timestamp

--- DASMAMO/DASMAMO_22AUG2017/src/calcws/Logging.java
+++ DASMAMO/DASMAMO_22AUG2017/src/calcws/Logging.java
@@ -1,5 +1,13 @@
 package calcws;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
@@ -9,6 +17,9 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Logging {
+	InetAddress thisIp;
+	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	
 	   /** 
      * This is the method which I would like to execute
      * before a selected method execution.
@@ -24,11 +35,38 @@ public class Logging {
   /** 
      * This is the method which I would like to execute
      * after a selected method execution.
+ * @throws IOException 
   */
 
 	   @After("selectAll()")
-  public void afterAdvice(){
+  public void afterAdvice() throws IOException{
      System.out.println("Results have been posted");
+     System.out.println( thisIp.getLocalHost());
+     //method 2 - via Date
+     Date date = new Date();
+     System.out.println(new Timestamp(date.getTime()));
+/*
+     //logfile
+     Logger logger = Logger.getLogger("MyLog");  
+     FileHandler fh;  
+
+     try {  
+
+         // This block configure the logger with handler and formatter  
+         fh = new FileHandler("MyLogFile.log");  
+         logger.addHandler(fh);
+         SimpleFormatter formatter = new SimpleFormatter();  
+         fh.setFormatter(formatter);  
+
+         // the following statement is used to log any messages  
+         logger.info(thisIp.getHostName() + " " + thisIp.getLocalHost() + this);  
+
+     } catch (SecurityException e) {  
+         e.printStackTrace();  
+     } catch (IOException e) {  
+         e.printStackTrace();  
+     }  */
+
   }
 
   /** 
