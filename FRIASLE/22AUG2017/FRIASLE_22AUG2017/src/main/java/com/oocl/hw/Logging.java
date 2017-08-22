bb489aeaaac104cfc1bfce4d54031186a91dfcaf
FoundationPig <leviemmanuelfrias@gmail.com>
2017-08-22 17:58:19 +0800

    add Ip address, host, and date

--- FRIASLE/22AUG2017/FRIASLE_22AUG2017/src/main/java/com/oocl/hw/Logging.java
+++ FRIASLE/22AUG2017/FRIASLE_22AUG2017/src/main/java/com/oocl/hw/Logging.java
@@ -1,15 +1,19 @@
 package com.oocl.hw;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.annotation.Before;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Logging {
+	
+	InetAddress thisIp;
    /** Following is the definition for a pointcut to select
       *  all the methods available. So advice will be called
       *  for all the methods.
@@ -52,4 +56,10 @@ public class Logging {
    public void AfterThrowingAdvice(IllegalArgumentException ex){
 		System.out.println("There has been an exception: " + ex.toString());
 	}
+   
+   @After("selectAll()")
+   public void afterAdviceTwo() throws UnknownHostException{
+      System.out.println("Operation done on: " + new java.util.Date());
+      System.out.println("Ip address and host is: " + InetAddress.getLocalHost());
+   }
 }
